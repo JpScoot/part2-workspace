1a90e1bac4c2c31e21a4f53b28fb71381581f86e
JpScoot <jamespjones0714@gmail.com>
2022-09-26 12:56:52 -0700

    Completed lab 5.1.

---
+++ Lab05.1/src/gov/irs/enumtype/IRS.java
@@ -0,0 +1,18 @@
+package gov.irs.enumtype;
+
+import gov.irs.TaxPayer;
+
+public interface IRS {
+
+  static IRS getInstance(){
+    return IRSEnum.INSTANCE;
+  }
+
+//business methods
+   void collectTaxes();
+
+  void register(TaxPayer payer);
+
+
+
+}
