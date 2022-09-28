851bf0fa92d6851d87fe70bdc62facac38b18423
JpScoot <jamespjones0714@gmail.com>
2022-09-27 19:30:08 -0700

    Completed Lab 5.3.

--- Lab05.3/src/com/javatunes/billing/USATax.java
+++ Lab05.3/src/com/javatunes/billing/USATax.java
@@ -15,6 +15,15 @@ package com.javatunes.billing;
  *  
  * TODO: implement this algorithm.
  */
-public class USATax {
+public class USATax implements TaxCalculator {
+  //declare fields
+  private final double RATE = 0.1;
+  private final double THRESHOLD = 20;
+
+
+  @Override
+  public double taxAmount(double taxable) {
+    return (taxable > THRESHOLD) ? (RATE * (taxable - 20)) : 0;
+  }
 
 }
